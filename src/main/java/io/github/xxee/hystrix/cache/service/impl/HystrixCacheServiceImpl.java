6e5cd679f1b03291995cc293a0b48146230dc4cb
dongxie <dongxie@xx>
2024-06-26 16:50:35 +0800

    v1.0.3:增加异常判断兜底

--- src/main/java/io/github/xxee/hystrix/cache/service/impl/HystrixCacheServiceImpl.java
+++ src/main/java/io/github/xxee/hystrix/cache/service/impl/HystrixCacheServiceImpl.java
@@ -1,13 +1,20 @@
 package io.github.xxee.hystrix.cache.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.CacheInvokeException;
 import io.github.xxee.hystrix.cache.service.HystrixCacheService;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.util.Objects;
 
 /**
  * Created by xiedong
  * Date: 2024/2/25 16:40
  */
+@Slf4j
 public class HystrixCacheServiceImpl implements HystrixCacheService {
     private Cache<String, Object> hystrixCache;
 
@@ -18,11 +25,28 @@ public class HystrixCacheServiceImpl implements HystrixCacheService {
 
     @Override
     public Object getFallbackData(String key) {
-        return hystrixCache.get(key);
+        if (StrUtil.isBlank(key)) {
+            return null;
+        }
+        try {
+            return hystrixCache.get(key);
+        } catch (Exception e) {
+            log.error("getFallbackData-error,key:{},err", key, e);
+            return null;
+        }
     }
 
     @Override
     public void putFallBackData(String key, Object val) {
-        hystrixCache.put(key, JSON.toJSONString(val));
+        if (StrUtil.isBlank(key)
+                || Objects.isNull(val)
+                || StrUtil.isBlank(val.toString())) {
+            return;
+        }
+        try {
+            hystrixCache.put(key, JSON.toJSONString(val));
+        } catch (Exception e) {
+            log.error("putFallBackData-error,key:{},err", key, e);
+        }
     }
 }
